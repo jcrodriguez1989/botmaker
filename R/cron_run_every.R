f989692cb0d602667f91892efbe2f4053cbc1706
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2021-09-16 16:57:30 -0300

    fixing cron

--- R/cron_run_every.R
+++ R/cron_run_every.R
@@ -7,12 +7,23 @@
 #' @param days Run every given days.
 #' @param months Run every given months.
 #'
+#' @importFrom glue glue
+#'
 #' @export
 #'
 cron_run_every <- function(minutes = NULL, hours = NULL, days = NULL, months = NULL) {
-  minutes <- ifelse(is.null(minutes), "*", paste0("*/", minutes))
-  hours <- ifelse(is.null(hours), "*", paste0("*/", hours))
-  days <- ifelse(is.null(days), "*", paste0("*/", days))
-  months <- ifelse(is.null(months), "*", paste0("*/", months))
-  paste(minutes, hours, days, months, "*")
+  if (length(c(minutes, hours, days, months)) > 1) {
+    stop("At most, one parameter can be provided.")
+  }
+  if (!is.null(months)) {
+    glue("0 0 1 */{months} *")
+  } else if (!is.null(days)) {
+    glue("0 0 */{days} * *")
+  } else if (!is.null(hours)) {
+    glue("0 */{hours} * * *")
+  } else if (!is.null(minutes)) {
+    glue("*/{minutes} * * * *")
+  } else {
+    "* * * * *"
+  }
 }
